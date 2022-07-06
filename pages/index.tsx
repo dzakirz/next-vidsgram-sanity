e5b0d098af6a73dbfc9fd2c74d6f66c82fdf35e6
Dzakirz <dzakirz.dev@gmail.com>
2022-07-06 08:47:47 +0700

    sidebar err

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
+    const testing = true
+
     return (
         <div className="text-3xl font-bold ">
-            <h1>Hello World</h1>
+            {testing && <h1>Hello World</h1>}
         </div>
     )
 }
